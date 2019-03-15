c639ffa3ac81c5bcf53ee047b59bf88973d9cfc4
Romilo903 <37272948+romilo903@users.noreply.github.com>
2019-03-15 22:47:44 +0000

    Update Index.js
    Terminado o evento de "ready" no arquivo principal (index.js)

--- JavaScript/Index.js
+++ JavaScript/Index.js
@@ -3,6 +3,12 @@ const config = require('./config.json');
 const client = new Discord.Client();
 client.prefix = config.prefix;
 
-client.on('ready' async => {})
+client.on('ready', async () => {
+  console.log('Bot iniciado');
+  console.log(client.user.tag + " - " + client.user.guild.size + " servidores, " + client.user.users.size + " usuários");
+
+  client.user.setActivity(client.prefix + "ajuda", { type: 'WATCHING' });
+});
+
 
 client.login(config.token);
