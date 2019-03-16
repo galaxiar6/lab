b4ca7b4bdf0507b42b4933e3d7d7ab30647b5f83
Romilo903 <37272948+romilo903@users.noreply.github.com>
2019-03-16 11:46:55 +0000

    Update Index.js
    O código está pronto para receber comandos!! Use `!addcmd` para enviar o seu comando!!

--- JavaScript/Index.js
+++ JavaScript/Index.js
@@ -6,9 +6,25 @@ client.prefix = config.prefix;
 client.on('ready', async () => {
   console.log('Bot iniciado');
   console.log(client.user.tag + " - " + client.user.guild.size + " servidores, " + client.user.users.size + " usuários");
-
   client.user.setActivity(client.prefix + "ajuda", { type: 'WATCHING' });
 });
 
+client.on('message', async message => {
+  if (message.author.bot) return undefined;
+  if (message.channel.type === 'dm') return undefined;
+  let msg = message.content.toLowerCase();
+  
+  if (msg.indexOf(client.prefix) !== 0) return undefined;
+  const args = message.content.slice(client.prefix.length).trim().split(/ +/g);
+  const comando = args.shift().toLowerCase();
+  
+  try {
+    delete require.cache[require.resolve(`./comandos/${comando}.js`)] 
+    let command = require(`./comandos/${comando}.js`);
+    command.run(client, message, args);
+  } catch (e) {
+   console.log(e);
+  } finally {};
+});
 
 client.login(config.token);
